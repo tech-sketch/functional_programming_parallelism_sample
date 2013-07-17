7d045754363aaa7768e2732dba13cd56ccab120f
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2013-07-17 18:10:29 +0900

    project.cljを修正

--- clojure/par2/project.clj
+++ clojure/par2/project.clj
@@ -1,8 +1,8 @@
 (defproject par2 "0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "clojure parallelism sample (clojure.core.reducers)"
+  :url "http://tech-sketch.jp"
+  :license {:name "GNU GPL V3"
+            :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.atilika.kuromoji/kuromoji "0.7.7"]]
   :repositories [["Atilika Open Source repository" "http://www.atilika.org/nexus/content/repositories/atilika"]]
